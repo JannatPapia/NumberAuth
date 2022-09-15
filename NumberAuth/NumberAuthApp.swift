9405a6037352efef171e7a069e9f830bcbe40352
PapiaMCode <papiamcode@gmail.com>
2022-09-15 21:47:28 +0600

    Initial Commit

---
+++ NumberAuth/NumberAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberAuthApp.swift
+//  NumberAuth
+//
+//  Created by Jannatun Nahar Papia on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct NumberAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
